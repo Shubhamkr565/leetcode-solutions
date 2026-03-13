cd160c243c9316d14fc9bd8f224f373c4e1c4001
Shubham <shubhamkr565@gmail.com>
2026-03-13 17:03:37 +0530

    DSA_Series_in_Python

---
+++ DSA_Series_in_Python/MajorityElement.py
@@ -0,0 +1,27 @@
+"""Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+"""
+
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+            
+        return candidate
+    
+
+if __name__ == "__main__":
+    # Example usage:
+    nums = [2, 2, 1, 1, 1, 2, 2]
+    print("Input:", nums)
+    print("Majority element:", Solution().majorityElement(nums))
